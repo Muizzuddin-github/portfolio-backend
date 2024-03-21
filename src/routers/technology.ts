082ae23437d8181d7575a48c03dcd00235643bdc
unknown <muizzuddin334@gmail.com>
2024-03-21 10:04:55 +0700

    testing

--- src/routers/technology.ts
+++ src/routers/technology.ts
@@ -6,18 +6,8 @@ import upload from "../utility/multer";
 const technology: express.Router = express.Router();
 
 technology.get("/api/technology", Technology.getAll);
-technology.post(
-  "/api/technology",
-  onlyLogin,
-  upload.single("logo"),
-  Technology.Add
-);
-technology.put(
-  "/api/technology/:id",
-  onlyLogin,
-  upload.single("logo"),
-  Technology.Edit
-);
-technology.delete("/api/technology/:id", onlyLogin, Technology.Del);
+technology.post("/api/technology", upload.single("logo"), Technology.Add);
+technology.put("/api/technology/:id", upload.single("logo"), Technology.Edit);
+technology.delete("/api/technology/:id", Technology.Del);
 
 export default technology;

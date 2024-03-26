1b147a7c4a9df27b38b6ee96e55b9b184dc3aa51
unknown <muizzuddin334@gmail.com>
2024-03-26 10:55:46 +0700

    protect project history

--- src/routers/projectHistory.ts
+++ src/routers/projectHistory.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import ProjectHistory from "../controllers/projectHistory";
 import upload from "../utility/multer";
+import onlyLogin from "../middlewares/onlyLogin";
 
 const projectHistory: express.Router = express.Router();
 
@@ -8,6 +9,7 @@ projectHistory.get("/api/project-history", ProjectHistory.getAll);
 projectHistory.post(
   "/api/project-history",
   upload.single("image"),
+  onlyLogin,
   ProjectHistory.Add
 );
 
